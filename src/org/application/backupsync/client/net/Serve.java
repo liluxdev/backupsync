37728e3ea67033cd7522c39b69d387a3ab8681e2
Enrico Bianchi <enrico.bianchi@ymail.com>
2011-12-02 11:06:17 +0100

    Removed unused libraries Client: factorized code

--- src/org/application/backupsync/client/net/Serve.java
+++ src/org/application/backupsync/client/net/Serve.java
@@ -21,7 +21,9 @@ import org.json.JSONObject;
 public class Serve implements AutoCloseable {
 
     private Integer port;
+    private BufferedReader in;
     private ServerSocket socket;
+    private PrintWriter out;
 
     public Serve(Integer port) {
         this.port = port;
@@ -29,8 +31,7 @@ public class Serve implements AutoCloseable {
 
     public Boolean listen() throws UnknownHostException, IOException {
         Boolean exit;
-        BufferedReader in;
-        PrintWriter out;
+
         Socket connection;
         JSONObject inJSON, outJSON, errJSON;
 
@@ -42,47 +43,61 @@ public class Serve implements AutoCloseable {
 
         try {
             inJSON = new JSONObject(in.readLine());
-            switch (inJSON.getString("command")) {
-                case "exit":
-                    exit = Boolean.TRUE;
+            switch (inJSON.getString("context")) {
+                case "file":
+                    switch (inJSON.getString("command")) {
+                        case "list":
+                            // TODO: write code for list files and directories (remember to calculate MD5 hash)
+                            outJSON = new FetchFile(inJSON.getString("directory"), inJSON.getBoolean("acl")).getJSON();
+                            outJSON.append("result", "ok");
+                            out.println(outJSON.toString());
+                            break;
+                        default:
+                            errJSON = new JSONObject();
+                            errJSON.append("result", "error");
+                            errJSON.append("message", "Command not found");
+                            out.println(errJSON.toString());
+                            break;
+                    }
                     break;
-                case "list":
-                    // TODO: write code for list files and directories (remember to calculate MD5 hash)
-                    outJSON = new FetchFile(inJSON.getString("directory"), inJSON.getBoolean("acl")).getJSON();
-                    outJSON.append("result", "ok");
-                    out.println(outJSON.toString());
+                case "system":
+                    switch (inJSON.getString("command")) {
+                        case "exit":
+                            exit = Boolean.TRUE;
+                            break;
+                        default:
+                            errJSON = new JSONObject();
+                            errJSON.append("result", "error");
+                            errJSON.append("message", "Command not found");
+                            out.println(errJSON.toString());
+                            break;
+                    }
+                    break;
+                default:
+                    errJSON = new JSONObject();
+                    errJSON.append("result", "error");
+                    errJSON.append("message", "Context not found");
+                    out.println(errJSON.toString());
                     break;
             }
-        } catch (JSONException ex) {
+        } catch (JSONException | FileNotFoundException | IllegalArgumentException | NullPointerException ex) {
             try {
                 errJSON = new JSONObject();
                 errJSON.append("result", "error");
-                errJSON.append("message", "Malformed command");
+                
+                if (ex instanceof FileNotFoundException ||
+                    ex instanceof IllegalArgumentException) {
+                    errJSON.append("message", "File or path not found");
+                } else if (ex instanceof NullPointerException) {
+                    errJSON.append("message", "Buffer error");
+                } else {
+                    errJSON.append("message", "Malformed command");
+                }
                 errJSON.append("error", ex.getCause());
-                out.println(errJSON.toString());
-            } catch (JSONException ex2) {
-            }
-        } catch (FileNotFoundException ex) {
-            try {
-                errJSON = new JSONObject();
-                errJSON.append("result", "error");
-                errJSON.append("message", "File or path not found");
-                errJSON.append("error", ex.getCause());
-                out.println(errJSON.toString());
-            } catch (JSONException ex2) {
-            }
-        } catch (NullPointerException ex) {
-            try {
-                errJSON = new JSONObject();
-                errJSON.append("result", "error");
-                errJSON.append("message", "Buffer error");
                 out.println(errJSON.toString());
             } catch (JSONException ex2) {
             }
         }
-
-        in.close();
-        out.close();
         connection.close();
 
         return exit;
@@ -93,6 +108,8 @@ public class Serve implements AutoCloseable {
     }
 
     public void close() throws IOException {
-        socket.close();
+        this.in.close();
+        this.out.close();
+        this.socket.close();
     }
 }
